8cd8bd855d77e9eaa371f8312ebeb854c95b020f
ThEsposito <thesposito.estudos@gmail.com>
2025-07-22 14:41:37 -0300

    Reestruturação e versão final do projeto

--- Api/usando_python/pokemons2.py
+++
@@ -1,6 +0,0 @@
-"""
-Nesse arquivo, vou utilizar uma abordagem diferente, chamando primeiro o json com 
-todos os pokemon da gen1 pra verificar o tipo da resposta
-
-
-"""
--- Sources/Pokedex_SwiftUI/main.swift
+++
@@ -1,6 +0,0 @@
-PokemonAPI().fetchPokemons { pokemons in
-    for pokemon in pokemons {
-        print("Pokemon: \(pokemon.name)")
-        print("URL: \(pokemon.url)")
-    }
-}
